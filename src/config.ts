627d62ef4ccf933d0ad28d3f4797ca49b4d093de
Victor Arsjad <victorarsjad@gmail.com>
2026-01-05 15:52:57 +0700

    update config, remove twitter logo

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const siteConfig = {
     github: "https://github.com/victorarsjad",
   },
   aboutMe:
-    "Software Engineer with 6+ years of professional experience in software development, proficient in using technologies such as Go, Kafka, MongoDB. Skilled in leading initiatives, refining workflows, and resolving critical issues efficiently. Possesses a strong track record of collaboration and adherence to best practices.",
+    "Software Engineer with 6+ years of professional experience in software development, proficient in using technologies such as Go, Kafka, Postgres. Skilled in leading initiatives, refining workflows, and resolving critical issues efficiently. Possesses strong track record of deliveries, collaboration, and adherence to best practices.",
   skills: ["Go", "Kubernetes"],
   experience: [
     {
@@ -17,10 +17,10 @@ export const siteConfig = {
       title: "Senior Software Engineer",
       dateRange: "Jan 2022 - Present",
       bullets: [
-        "Led technical initiatives that reduced merchant onboarding processing time by 20%, improving conversion rates for over 100k merchants.",
-        "Led system architecture redesign initiatives, including ADR documentation and cross-team technical alignment.",
-        "Owned and maintained critical data storage service supporting 330M+ daily transactions across GoTo's ecosystem.",
-        "Led incident response team, maintaining 99.99% service uptime for merchant-critical systems."
+        "Led technical initiatives that reduced merchant onboarding processing time by 20%, improving conversion rates for over 100k merchants",
+        "Led system architecture redesign initiatives, including ADR documentation and cross-team technical alignment",
+        "Owned and maintained critical data storage service supporting 330M+ daily transactions across GoTo's ecosystem",
+        "Led incident response team, maintaining 99.99% service uptime for merchant-critical systems"
       ],
     },
     {
@@ -28,19 +28,19 @@ export const siteConfig = {
       title: "Software Engineer",
       dateRange: "Dec 2019 - Oct 2020",
       bullets: [
-        "Developed a robust, scalable backend service using Spring Boot and Kotlin to power Moka merchant lending platform.",
-        "Integrated backend capital service with internal reporting and payment service, and third-party peer-to-peer lending providers, ensuring seamless end-to-end functionality.",
-        "Collaborated closely with cross-functional teams, including Product, UI/UX, and QA, to gather requirements, prioritize features, and ensure timely delivery.",
+        "Developed a robust, scalable backend service using Spring Boot and Kotlin to power Moka merchant lending platform",
+        "Integrated backend capital service with internal reporting and payment service, and third-party peer-to-peer lending providers, ensuring seamless end-to-end functionality",
+        "Collaborated closely with cross-functional teams, including Product, UI/UX, and QA, to gather requirements, prioritize features, and ensure timely delivery",
       ],
     },
     {
       company: "eComindo",
       title: "Associate Developer",
-      dateRange: "Aug 2018 - May 2020",
+      dateRange: "Aug 2018 - Dec 2019",
       bullets: [
-        "Responsible for integration of client-side services with Allianz internal components.",
-        "Develop web application using Angular.",
-        "Develop RESTful API service using Java with Spring Framework.",
+        "Responsible for integration of client-side services with Allianz internal components",
+        "Develop web application using Angular",
+        "Develop RESTful API service using Java with Spring Framework",
       ],
     },
   ],
